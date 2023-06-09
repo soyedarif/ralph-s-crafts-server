dbc75891004892e8a56daa8a6f518750ec28ca61
soyedarif <soyedariff@gmail.com>
2023-06-09 20:47:12 +0600

    users collection created

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const express= require('express')
 const app=express();
 const cors= require('cors')
+require("dotenv").config();
 const port=process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2l3te9a.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //users
+    const usersCollection = client.db("ralphCrafsDB").collection("users");
+
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await usersCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: "user already exists" });
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/',(req,res)=>{
     res.send('ralph is on the run')
