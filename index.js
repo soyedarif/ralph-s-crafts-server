118ed9fbf9fdeb3d730ae59fd7f8538b53a64f43
soyedarif <soyedariff@gmail.com>
2023-06-11 17:29:38 +0600

    verify admin and instructor patch added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -19,8 +19,8 @@ const verifyJWT = (req, res, next) => {
     if (err) {
       return res.status(401).send({ error: true, message: "unauthorized access" });
     }
-    req.decoded=decoded;
-    next()
+    req.decoded = decoded;
+    next();
   });
 };
 
@@ -48,8 +48,19 @@ async function run() {
       res.send({ token });
     });
 
-    app.get("/users",verifyJWT, async (req, res) => {
+    app.get("/users", verifyJWT, async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
       res.send(result);
     });
 
@@ -60,7 +71,32 @@ async function run() {
       if (existingUser) {
         return res.send({ message: "user already exists" });
       }
+      user.role = "student";
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateRole = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateRole);
+      res.send(result);
+    });
+    
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateRole = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateRole);
       res.send(result);
     });
 
